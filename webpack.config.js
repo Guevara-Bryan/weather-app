743b8b8fda11c7d1e93216f90a273c2092912f2a
Bryan Guevara <bryan19052000@gmail.com>
2022-06-21 20:20:02 -0700

    Updated config for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: { 
-        static: './dist',
-    },
     plugins:[
         new HtmlWebpackPlugin({
             title: 'Weather App - Dev',
